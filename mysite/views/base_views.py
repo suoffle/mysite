836d2ff2a41d8e21833dd68460ba8d632e094cf4
이수아 <dltndk332@gmail.com>
2025-02-13 15:17:21 +0900

    log 3/0 re

--- mysite/views/base_views.py
+++ mysite/views/base_views.py
@@ -7,9 +7,7 @@ logger = logging.getLogger('mysite')
 from ..models import Question
 
 def index(request):
-    logger.info("INFO 레벨로 출력")
-
-def index(request):
+    3/0
     page=request.GET.get('page',1)
     kw=request.GET.get('kw','')
     question_list = Question.objects.order_by('-create_date')
